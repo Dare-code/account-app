2fb972248d76fa8a2143652b77a97c3123ce049e
Darko Milevski <milevskidarko89@gmail.com>
2022-08-09 22:50:12 +0200

    added index

--- src/components/Invoices/Invoices.js
+++ src/components/Invoices/Invoices.js
@@ -3,16 +3,16 @@ import "./Invoices.css";
 import Invoice from "../Invoice/Invoice";
 
 const Invoices = ({ companies }) => {
-  return (
-    <>
-      {companies.map((invoice, i) => {
-        return (
-          <div className="invoice">
-            <Invoice key={i} invoice={invoice} />
-          </div>
-        );
-      })}
-    </>
-  );
+    return (
+        <>
+            {companies.map((invoice, i) => {
+                return (
+                    <div className="invoice" key={i}>
+                        <Invoice invoice={invoice} />
+                    </div>
+                );
+            })}
+        </>
+    );
 };
 export default Invoices;
